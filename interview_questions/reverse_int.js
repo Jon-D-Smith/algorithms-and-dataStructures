241c9005f280e5adf59ace1420cac97eef015817
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-11 22:26:56 -0500

    Reverse int method now handles negative numbers

--- interview_questions/reverse_int.js
+++ interview_questions/reverse_int.js
@@ -1,6 +1,8 @@
 function reverseInt(int) {
-    return parseInt(int.toString().split('').reverse().join(''))
+    return parseInt(int.toString().split('').reverse().join('')) * Math.sign(int)
 }
 
 //Expects 522
 console.log(reverseInt(225))
+//expects -522
+console.log(reverseInt(-225))
