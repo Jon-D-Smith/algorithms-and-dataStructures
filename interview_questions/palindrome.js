c661f6f63c09f2894f68138204c9331c1538c784
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-11 22:19:48 -0500

    Got functional palindrome checker with built in Javascript Array methods

---
+++ interview_questions/palindrome.js
@@ -0,0 +1,8 @@
+//Original palindrome check method with built in array methods
+function isPalindromeA(str) {
+    return str == str.split('').reverse().join('')
+}
+
+
+
+console.log(isPalindrome("wow"))
