60064df65c2a649f02bbe5a923e23a3c2e7bcfb8
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-14 22:53:37 -0500

    Added proper variable denotation

--- binary_search.js
+++ binary_search.js
@@ -1,11 +1,11 @@
 array = [1, 3, 9, 12, 15, 21, 67, 89, 120]
 
 function binarySearch(array, search_value) {
-    lower_value = 0
-    upper_value = array.length - 1
+    let lower_value = 0
+    let upper_value = array.length - 1
     while(lower_value <= upper_value) {
-        mid_value = Math.floor((upper_value + lower_value) / 2)
-        value_at_midpoint = array[mid_value]
+        let mid_value = Math.floor((upper_value + lower_value) / 2)
+        let value_at_midpoint = array[mid_value]
 
         if(search_value == value_at_midpoint){
             return `your value is at ${mid_value}`
