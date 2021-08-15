fe0dcdcb7185533a6f70c1f569264d287c4afb72
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-15 17:30:25 -0500

    Added a faster implimentation of a duplication finder

--- duplicate.js
+++ duplicate.js
@@ -1,5 +1,6 @@
 const array = [1,5,3,7,9,7,6]
 
+// This function is O(n2) making it a slow algorithm
 function hasDuplicate(array) {
     for(let i = 0; i < array.length; i++) {
         for(let j = 0; j < array.length; j++){
@@ -13,4 +14,19 @@ function hasDuplicate(array) {
     return 'No duplicates found'
 }
 
+// This function is O(N) making it linear. It is much faster than the previous approach
+function hasDuplicateValue(array){
+    let existingNumbers = []
+    for(let i = 0; i < array.length; i++) {
+        if(existingNumbers[array[i]] === 1) {
+            return `Duplicate found`
+        } else {
+            existingNumbers[array[i]] = 1
+        }
+
+    }
+    return `No duplicates found`
+}
+
 console.log(hasDuplicate(array))
+console.log(hasDuplicateValue(array))
