8194e8580df617e6bb5597f76c9d197a9d4d8b07
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-14 22:52:45 -0500

    Finished binary search

---
+++ binary_search.js
@@ -0,0 +1,23 @@
+array = [1, 3, 9, 12, 15, 21, 67, 89, 120]
+
+function binarySearch(array, search_value) {
+    lower_value = 0
+    upper_value = array.length - 1
+    while(lower_value <= upper_value) {
+        mid_value = Math.floor((upper_value + lower_value) / 2)
+        value_at_midpoint = array[mid_value]
+
+        if(search_value == value_at_midpoint){
+            return `your value is at ${mid_value}`
+        }else if(search_value < value_at_midpoint) {
+            upper_value = mid_value - 1
+        } else if(search_value > value_at_midpoint){
+            lower_value = mid_value + 1
+        }
+    }
+    
+
+    return `Value not found`
+}
+
+console.log(binarySearch(array, 21))
