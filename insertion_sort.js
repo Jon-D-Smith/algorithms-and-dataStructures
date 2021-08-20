8314f2833e55e13ee6c461d211adfbeff15e3d54
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-20 15:38:32 -0500

    Added insertion sort

---
+++ insertion_sort.js
@@ -0,0 +1,22 @@
+const array = [4,2,7,1,3]
+
+function insertionSort(array) {
+    for(let i = 0; i < array.length; i++) {
+        let temp_value = array[i]
+        let position = i - 1
+
+        while(position >= 0) {
+            if(array[position] > temp_value) {
+                array[position + 1] = array[position]
+                position = position - 1
+            }
+            else {
+                break
+            }
+        }
+        array[position + 1] = temp_value
+    }
+    return array
+}
+
+console.log(insertionSort(array))
