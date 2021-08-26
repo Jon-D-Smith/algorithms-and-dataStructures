114aee57cd7febeb03d91e87795cd1320db6a237
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-25 22:09:47 -0500

    Added a faster algorithm for the palindrome checker

--- interview_questions/palindrome.js
+++ interview_questions/palindrome.js
@@ -4,3 +4,22 @@ function isPalindromeA(str) {
 }
 
 console.log(isPalindromeA("racecar"));
+
+// Another method for Palindrome checking
+// This is a much faster method as it is N/2, and is noted in BigO as O(N)
+
+function isPalindromeB(str) {
+  let leftIndex = 0;
+  let rightIndex = str.length - 1;
+
+  while (leftIndex < str.length / 2) {
+    if (str[leftIndex] !== str[rightIndex]) {
+      return false;
+    }
+    leftIndex++;
+    rightIndex--;
+  }
+  return true;
+}
+
+console.log(isPalindromeB("deified"));
