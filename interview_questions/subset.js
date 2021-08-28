0dd1ed74a1255b48e0fc0f948d3d4ee65080dce5
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-27 21:19:04 -0500

    Fixed original snippet

--- interview_questions/subset.js
+++ interview_questions/subset.js
@@ -9,7 +9,7 @@ function isSubset(array1, array2) {
   let largerArray;
   let smallerArray;
 
-  if (array1 > array2) {
+  if (array1.length > array2.length) {
     largerArray = array1;
     smallerArray = array2;
   } else {
@@ -30,3 +30,29 @@ function isSubset(array1, array2) {
   }
   return true;
 }
+
+function isSubsetHash(array1, array2) {
+  let largerArray;
+  let smallerArray;
+  let hashTable = {};
+
+  if (array1.length > array2.length) {
+    largerArray = array1;
+    smallerArray = array2;
+  } else {
+    largerArray = array2;
+    smallerArray = array1;
+  }
+
+  for (const value of largerArray) {
+    hashTable[value] = true;
+  }
+
+  for (const value of smallerArray) {
+    if (hashTable[value]) {
+      return false;
+    }
+  }
+}
+
+console.log(isSubset(array1, array2));
