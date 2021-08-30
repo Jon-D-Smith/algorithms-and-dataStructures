634d5599092b095930beb257788569dac21c51ef
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-29 20:36:46 -0500

    Update longest word interview question to handle multiple longest words

--- interview_questions/longestWord.js
+++ interview_questions/longestWord.js
@@ -5,15 +5,29 @@ const string = "longest word in an array probably this one";
 function longestWord(string) {
   const words = string.split(" ");
   let longest = 0;
-  let winner;
+  let winners = [];
 
   for (const word in words) {
     if (words[word].length > longest) {
       longest = words[word].length;
-      winner = words[word];
+      winners = [words[word]];
+    } else if (words[word].length === longest) {
+      winners.push(words[word]);
     }
   }
-  return winner;
+  return winners.length == 1 ? winners[0] : winners;
 }
+
+// function longestWordObject(string) {
+//   const words = string.split(" ");
+//   let obj = {};
+//   for (const word in words) {
+//     obj[words[word]] = words[word].length;
+//   }
+//   for (const word in obj) {
+//     console.log(obj[word]);
+//   }
+//   //   console.log(obj);
+// }
 
 console.log(longestWord(string));
