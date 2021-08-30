51111f1f68264c75936d3a97f4d9871eddb75a28
Jon Smith <aardvarkjon94@yahoo.com>
2021-08-29 20:39:30 -0500

    Cleaned up code for longest word function

--- interview_questions/longestWord.js
+++ interview_questions/longestWord.js
@@ -1,4 +1,4 @@
-// Find the longest word of a given string (If there are multiple longest words, it is okay to return one)
+// Find the longest word of a given string (If there are multiple longest words, put them into an array)
 
 const string = "longest word in an array probably this one";
 
@@ -17,17 +17,5 @@ function longestWord(string) {
   }
   return winners.length == 1 ? winners[0] : winners;
 }
-
-// function longestWordObject(string) {
-//   const words = string.split(" ");
-//   let obj = {};
-//   for (const word in words) {
-//     obj[words[word]] = words[word].length;
-//   }
-//   for (const word in obj) {
-//     console.log(obj[word]);
-//   }
-//   //   console.log(obj);
-// }
 
 console.log(longestWord(string));
