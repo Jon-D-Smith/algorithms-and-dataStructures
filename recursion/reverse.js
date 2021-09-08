630075b5561b605d2598635e2ea3c0f0685f7136
Jon Smith <aardvarkjon94@yahoo.com>
2021-09-08 13:06:29 -0500

    Added ecursive string reversal algorithm

---
+++ recursion/reverse.js
@@ -0,0 +1,13 @@
+// Reverse a given string recursively
+
+const string = "abcde"
+
+function reverse(string, newStr=[], i=0){
+    if(i == string.length){
+        return string[i]
+    }
+    newStr[i] = string[string.length - (i+1)]
+    reverse(string, newStr, i+=1)
+    return newStr
+}
+console.log(reverse(string))
